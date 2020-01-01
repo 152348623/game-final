af0447c4e08907d831e39c9bb3b00f0217172fb7
152348623 <47877593+152348623@users.noreply.github.com>
2020-01-02 01:11:05 +0800

    修改
    修改trigger
    把canvas的畫面調整

--- final project/Assets/script/PlayerDeform.cs
+++ final project/Assets/script/PlayerDeform.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerDeform : MonoBehaviour
 {
     public GameObject playerObj;
-
+    float distance;
     public GameObject[] deformArray = new GameObject[4];   // 存變身物件
     // Start is called before the first frame update
     void Start()
     {
-        
+        distance = 1000f;
     }
 
     // Update is called once per frame
@@ -19,20 +20,50 @@ public class PlayerDeform : MonoBehaviour
         KeyDown();
     }
 
+    private bool IsSmallDistanceOfDeform(GameObject player, GameObject deform)   // 判斷player與範圍內的deformObj之間的距離
+    {
+        float temp = 0f;
+        temp = Vector3.Distance(player.transform.position, deform.transform.position);
+
+        if (temp <= distance)
+        {
+            distance = temp;
+            return true;
+        }
+        return false;
+    }
+
     private void OnTriggerStay(Collider other)
     {
         GameObject deformObj;
         deformObj = other.gameObject;
-        if (deformObj.tag == "deform") 
+
+        bool isCanCatch = false;
+
+        if (deformObj.tag == "deform")
         {
-            deformObj.GetComponent<cakeslice.Outline>().color = 1;  // 顯示外框
+            if (!deformObj.GetComponent<cakeslice.Outline>())   // 給deform outline的script
+                deformObj.AddComponent<cakeslice.Outline>();
+
+            if (IsSmallDistanceOfDeform(playerObj, deformObj))      //  判斷如果都在範圍內 取距離最短的deformObj顯示外框 並可以抓取
+            {
+                deformObj.GetComponent<cakeslice.Outline>().color = 1;  // 顯示外框
+                isCanCatch = true;
+            }
+            else
+            {
+                deformObj.GetComponent<cakeslice.Outline>().color = 0;  // 隱藏外框
+                distance = 100f;
+                isCanCatch = false;
+            }
         }
 
-        if (deformObj.tag == "deform" && Input.GetKey(KeyCode.F))  //碰到物件 才變身    F:獲取變身物件
+
+        if (deformObj.tag == "deform" && Input.GetKeyUp(KeyCode.F))  //碰到物件 可以獲得變身物件    F:獲取變身物件
         {
             for (int i = 0; i < 4; i++)
             {
-                if (deformArray[i] == null)
+                if (deformArray[i] == null && isCanCatch)
                 {
                     deformArray[i] = deformObj;   // 存放變身物件
                     deformArray[i].transform.parent = playerObj.transform;
@@ -42,15 +73,17 @@ public class PlayerDeform : MonoBehaviour
                 }
             }
         }
+
     }
 
     private void OnTriggerExit(Collider other)
     {
         GameObject deformObj;
         deformObj = other.gameObject;
-        if (deformObj.tag == "deform")
+        if (deformObj.tag == "deform")  // 離開範圍外 將外框隱藏
         {
-            deformObj.GetComponent<cakeslice.Outline>().color = 0;  
+            deformObj.GetComponent<cakeslice.Outline>().color = 0;
+            distance = 100f;
         }
     }
 
@@ -75,13 +108,15 @@ public class PlayerDeform : MonoBehaviour
             }
         }
 
-        if (playerObj.transform.GetChild(1).gameObject.activeSelf!=false)    // 可以變身物件 按1~4 可以變身    1~4 : 變身
+        if (playerObj.transform.GetChild(1).gameObject.activeSelf != false)    // 可以變身物件 按1~4 可以變身    1~4 : 變身
         {
             void SetObjActive(int index)
             {
                 playerObj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
                 playerObj.transform.GetChild(1).gameObject.SetActive(false);    // 原本人物
                 deformArray[index].SetActive(true);
+
+                playerObj.AddComponent<cakeslice.Outline>();
                 deformArray[index].GetComponent<cakeslice.Outline>().color = 0; // 將物體的外框去除
             }
 
@@ -89,7 +124,7 @@ public class PlayerDeform : MonoBehaviour
             {
                 SetObjActive(0);
             }
-            else if(Input.GetKey(KeyCode.Alpha2) && deformArray[1] != null)
+            else if (Input.GetKey(KeyCode.Alpha2) && deformArray[1] != null)
             {
                 SetObjActive(1);
             }
