e812da8c03b83d10bb8d97714272714f268e4e26
kukuman98 <khaiyi5893@gmail.com>
2020-01-05 01:37:52 +0800

    UI update

---
+++ final project/Assets/UI/getImageUimission.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class getImageUimission : MonoBehaviour
+{
+    //public GameObject playerObj;
+    public Image[] image;
+    public Image targetImage;
+    public GameObject playerObj;
+    private GameObject none;
+    private GameObject HolyWater;
+    private GameObject Diamond;
+    private GameObject Mantra;
+    private GameObject SacrificialOffering;
+    private GameObject[] itemArray;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        none = targetImage.transform.GetChild(0).gameObject;
+        HolyWater = targetImage.transform.GetChild(1).gameObject;
+        Diamond = targetImage.transform.GetChild(2).gameObject;
+        Mantra = targetImage.transform.GetChild(3).gameObject;
+        SacrificialOffering = targetImage.transform.GetChild(4).gameObject;
+
+        //set image initial
+        image[0].GetComponent<Image>().sprite = none.GetComponent<SpriteRenderer>().sprite;
+        image[1].GetComponent<Image>().sprite = none.GetComponent<SpriteRenderer>().sprite;
+        image[2].GetComponent<Image>().sprite = none.GetComponent<SpriteRenderer>().sprite;
+        image[3].GetComponent<Image>().sprite = none.GetComponent<SpriteRenderer>().sprite;
+        //0.none 1.apple 2.barrel 3.canned 4.carrot 5.fish 6.meat 7.mushroom
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        itemArray = playerObj.GetComponent<PlayerMission>().missionArray;
+
+        for(int i = 0; i < 4; i++)
+        {
+            if (itemArray[i] != null)
+            {
+                switch (itemArray[i].ToString())
+                {
+                    case "HolyWater (UnityEngine.GameObject)":
+                        image[i].GetComponent<Image>().sprite = HolyWater.GetComponent<SpriteRenderer>().sprite;
+                        break;
+                    case "Diamond (UnityEngine.GameObject)":
+                        image[i].GetComponent<Image>().sprite = Diamond.GetComponent<SpriteRenderer>().sprite;
+                        break;
+                    case "Mantra (UnityEngine.GameObject)":
+                        image[i].GetComponent<Image>().sprite = Mantra.GetComponent<SpriteRenderer>().sprite;
+                        break;
+                    case "SacrificialOffering (UnityEngine.GameObject)":
+                        image[i].GetComponent<Image>().sprite = SacrificialOffering.GetComponent<SpriteRenderer>().sprite;
+                        break;
+                    default:    //none
+                        image[i].GetComponent<Image>().sprite = none.GetComponent<SpriteRenderer>().sprite;
+                        break;
+                }
+            }
+            else
+            {
+                image[i].GetComponent<Image>().sprite = none.GetComponent<SpriteRenderer>().sprite;
+            }
+        }
+    }
+}
--- final project/Assets/UI/pauseGame.cs
+++ final project/Assets/UI/pauseGame.cs
@@ -8,12 +8,27 @@ using UnityEngine.SceneManagement;
 public class pauseGame : MonoBehaviour
 {
     public GameObject pannelUI;
+    public GameObject pannelMission;
+    public GameObject pannelContent;
     public GameObject player;
     private bool isPause;
     // Start is called before the first frame update
     void Start()
     {
         isPause = false;
+        
+    }
+    void initial()
+    {
+        Time.timeScale = 1;
+        pannelUI.SetActive(false);
+        pannelMission.SetActive(false);
+        pannelContent.SetActive(false);
+        isPause = false;
+        pannelContent.transform.GetChild(1).transform.GetChild(0).gameObject.SetActive(false);
+        pannelContent.transform.GetChild(1).transform.GetChild(1).gameObject.SetActive(false);
+        pannelContent.transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(false);
+        pannelContent.transform.GetChild(1).transform.GetChild(3).gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,9 +43,7 @@ public class pauseGame : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Escape) && isPause == true)
         {
-            Time.timeScale = 1;
-            isPause = false;
-            pannelUI.SetActive(isPause);
+            initial();
         }
     }
     public void resume()
@@ -45,10 +58,15 @@ public class pauseGame : MonoBehaviour
         int scene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(scene);
     }
-    public void back()
+    public void home()
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(0);
+    }
+    public void mission()
+    {
+        pannelUI.SetActive(false);
+        pannelMission.SetActive(true);
     }
     public void exit()
     {
@@ -60,5 +78,43 @@ public class pauseGame : MonoBehaviour
             Application.Quit();
         #endif
 
+    }
+    public void mission1()
+    {
+        pannelMission.SetActive(false);
+        pannelContent.SetActive(true);
+        pannelContent.transform.GetChild(1).transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void mission2()
+    {
+        pannelMission.SetActive(false);
+        pannelContent.SetActive(true);
+        pannelContent.transform.GetChild(1).transform.GetChild(1).gameObject.SetActive(true);
+    }
+    public void mission3()
+    {
+        pannelMission.SetActive(false);
+        pannelContent.SetActive(true);
+        pannelContent.transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(true);
+    }
+    public void mission4()
+    {
+        pannelMission.SetActive(false);
+        pannelContent.SetActive(true);
+        pannelContent.transform.GetChild(1).transform.GetChild(3).gameObject.SetActive(true);
+    }
+    public void backMenu()
+    {
+        pannelMission.SetActive(false);
+        pannelUI.SetActive(true);
+    }
+    public void backMission()
+    {
+        pannelContent.SetActive(false);
+        pannelContent.transform.GetChild(1).transform.GetChild(0).gameObject.SetActive(false);
+        pannelContent.transform.GetChild(1).transform.GetChild(1).gameObject.SetActive(false);
+        pannelContent.transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(false);
+        pannelContent.transform.GetChild(1).transform.GetChild(3).gameObject.SetActive(false);
+        pannelMission.SetActive(true);
     }
 }
