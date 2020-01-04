8a3a91e6a65e686571b5301db78d290da3e2ed58
152348623 <47877593+152348623@users.noreply.github.com>
2020-01-05 00:26:40 +0800

    letter

---
+++ final project/Assets/UI/MissionUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionUI : MonoBehaviour
+{
+
+    public GameObject pannelUI;
+    public GameObject player;
+    private bool isPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void missionOne()
+    {
+
+    }
+    public void missionTwo()
+    {
+
+    }
+    public void missionThree()
+    {
+
+    }
+    public void missionFour()
+    {
+
+    }
+}
--- final project/Assets/UI/pauseGame.cs
+++ final project/Assets/UI/pauseGame.cs
@@ -61,4 +61,6 @@ public class pauseGame : MonoBehaviour
         #endif
 
     }
+    
+
 }
